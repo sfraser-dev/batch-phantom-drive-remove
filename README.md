812902e0e30c13e8dbdfecd33fa5d624c8fb53b9
Stewart Fraser <stewart.fraser@f-e-t.com>
2016-11-02 14:55:24 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+Batch file to remove a "phantom drive", a drive which doesn't exist but which Windows has assigned a letter to.
---
+++ phantomDriveFix.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Removes phantom drives, drives which have been assigned a letter but which don't exist. 
+mountvol /R
