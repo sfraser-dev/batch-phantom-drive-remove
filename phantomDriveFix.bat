7103e9772d2191a816374f9333d5595e1f12151d
Stewart Fraser <stewart.fraser@f-e-t.com>
2016-11-04 10:36:30 +0000

    Update with more comments.

--- phantomDriveFix.bat
+++ phantomDriveFix.bat
@@ -1,3 +1,9 @@
 @echo off
 REM Removes phantom drives, drives which have been assigned a letter but which don't exist. 
+
+REM Can run these also (where drives I:, K: and N: are phantoms), command line may moan, but it will then allow the "mountvol /R" to work.
+REM mountvol I: /D
+REM mountvol K: /D
+REM mountvol N: /D
+
 mountvol /R
